63468b873dde68609da102ebf6d439639207937a
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-21 22:51:33 -0300

    #Feature: Import koin implementation

--- app/src/main/java/com/gabriel/pokedex/application/Application.kt
+++ app/src/main/java/com/gabriel/pokedex/application/Application.kt
@@ -1,10 +1,19 @@
 package com.gabriel.pokedex.application
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // start Koin!
+        startKoin {
+            // Android context
+            androidContext(this@Application)
+            // modules
+            modules()
+        }
     }
 }
