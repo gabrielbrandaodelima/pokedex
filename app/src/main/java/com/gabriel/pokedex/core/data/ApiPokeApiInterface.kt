c723a339d166f9277aa92b72df5d738378707305
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-23 13:45:16 -0300

    #Feature: Created PokeApi fetch pokemons list function and PokemonListingResponse.kt

--- app/src/main/java/com/gabriel/pokedex/core/data/ApiPokeApiInterface.kt
+++ app/src/main/java/com/gabriel/pokedex/core/data/ApiPokeApiInterface.kt
@@ -1,8 +1,15 @@
 package com.gabriel.pokedex.core.data
 
+import com.gabriel.pokedex.core.domain.model.response.PokemonListingResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface ApiPokeApiInterface {
 
-
+    @GET("api/v2/pokemon/")
+    suspend fun fetchPokemonsList(
+        @Query("offset") page: Int
+    ): PokemonListingResponse
 
 }
