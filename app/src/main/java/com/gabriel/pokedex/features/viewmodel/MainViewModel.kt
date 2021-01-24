76002d2f6f9bf5231d0ad161d3e4402ecf6c2821
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-24 14:26:32 -0300

    #Feature: MainViewModel.kt inherits BaseViewModel

--- app/src/main/java/com/gabriel/pokedex/features/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/gabriel/pokedex/features/viewmodel/MainViewModel.kt
@@ -1,7 +1,8 @@
 package com.gabriel.pokedex.features.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.gabriel.pokedex.core.platform.BaseViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel : BaseViewModel() {
     // TODO: Implement the ViewModel
 }
