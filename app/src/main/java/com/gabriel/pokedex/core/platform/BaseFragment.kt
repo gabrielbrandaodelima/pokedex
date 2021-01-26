b4a27674104fc83e793d3a8f5d1a284757bd7616
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-26 02:51:52 -0300

    #Feature: Added Search View

--- app/src/main/java/com/gabriel/pokedex/core/platform/BaseFragment.kt
+++ app/src/main/java/com/gabriel/pokedex/core/platform/BaseFragment.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.gabriel.pokedex.features.viewmodel.PokedexViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 abstract class BaseFragment(@LayoutRes layoutIdRes: Int = 0) : Fragment(layoutIdRes) {
--- app/src/main/java/com/gabriel/pokedex/features/ui/view/main/MainFragment.kt
+++ app/src/main/java/com/gabriel/pokedex/features/ui/view/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.gabriel.pokedex.features.ui.view.main
 
 import android.os.Bundle
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
@@ -16,13 +17,15 @@ import com.gabriel.pokedex.features.viewmodel.PokedexViewModel
 
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : BaseFragment(R.layout.fragment_main), SearchView.OnQueryTextListener,
+    SearchView.OnCloseListener, androidx.appcompat.widget.SearchView.OnQueryTextListener {
 
 
     private val binding by viewBinding(FragmentMainBinding::bind)
 
     private var needsLoading = MutableLiveData(false)
     private var pokesList: List<Pokemon>? = null
+    private var searchView: SearchView? = null
 
     val viewModel: PokedexViewModel by viewModel()
     private var pokemonAdapter: PokemonAdapter? = null
@@ -30,7 +33,41 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyclerView()
         initViewModel()
+        setSearchClickListener()
 
+    }
+
+    private fun setSearchClickListener() {
+        binding?.searchView.setOnQueryTextListener(this)
+        binding.layoutSearchBar?.setOnClickListener {
+            handleSearchClick()
+        }
+    }
+
+    private fun handleSearchClick() {
+        binding?.apply {
+            if (searchView.isVisible()) {
+                hideSearchView()
+            } else {
+                showSearchView()
+            }
+        }
+    }
+
+    fun showSearchView() {
+        binding?.apply {
+            searchImgview.gone()
+            searchTxtview.gone()
+            searchView.visible()
+        }
+    }
+
+    fun hideSearchView() {
+        binding?.apply {
+            searchView.gone()
+            searchImgview.visible()
+            searchTxtview.visible()
+        }
 
     }
 
@@ -67,7 +104,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             observe(loading, {
                 it?.let { it1 -> manageProgress(it1) }
             })
-//            failure(failure, ::handleFailure)
+            failure(failure, ::handleFailure)
         }
         viewModel.fetchPokemonsList()
     }
@@ -111,6 +148,19 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             list?.toMutableList()?.let { pokesList?.toMutableList()?.addAll(it) }
             pokemonAdapter?.appendAll(list as ArrayList<Pokemon>)
         }
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return false
+    }
+
+    override fun onClose(): Boolean {
+        hideSearchView()
+        return true
     }
 
 
